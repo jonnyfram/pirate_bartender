606e897b2a32ff9afa1b75dabc7deeefc6217a7c
Jonny Fram <jonnyfram@gmail.com>
2017-04-06 08:53:53 +0000

    Complete project with random drink name

--- pirate_bartender_01.py
+++ pirate_bartender_01.py
@@ -1,20 +1,11 @@
 import random
 
-#drink = [] #- shouldn't have to define this first?
-
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
     "bitter": "Are ye a lubber who likes it bitter?",
     "sweet": "Would ye like a bit of sweetness with yer poison?",
     "fruity": "Are ye one for a fruity finish?",
-}
-
-answers = {"strong": False,
-            "salty": False,
-            "bitter": False,
-            "sweet": False,
-            "fruity": False,
 }
 
 ingredients = {
@@ -25,10 +16,72 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
-#print(answers)
+adjectives = [
+"Battleship",
+"Accident",
+"Accordion",
+"Accountant",
+"Achiever",
+"Acid",
+"Chain",
+"Chairlift",
+"Chairman",
+"Goodie",
+"Goose",
+"Gopher",
+"Kangaroo",
+"Karate",
+"Karen",
+"Kayak",
+"Kazoo",
+"Weedkiller",
+"Weekend",
+"Weekender"
+]
 
-def OrderDrink(answers):
+nouns = [
+    'People',
+    'History',
+    'Way',
+    'Art',
+    'World',
+    'Map',
+    'Family',
+    'Government',
+    'Health',
+    'System',
+    'Computer',
+    'Meat',
+    'Year',
+    'Music',
+    'Person',
+    'Reading',
+    'Method',
+    'Data',
+    'Understanding',
+    'Theory',
+    'Law',
+    'Bird',
+    'Literature',
+    'Problem',
+    'Software',
+    'Control',
+    'Knowledge',
+    'Power',
+    'Ability',
+    'Economics',
+    'Love'
+]
+
+def RandomDrinkName():
+   adjective = random.choice(adjectives)
+   noun = random.choice(nouns)
+   drink_name = (str(adjective)+" "+str(noun))
+   return drink_name
     
+def OrderDrink():
+    
+    answers = {}
     for key, question in questions.items():
         
         answer = input(question)
@@ -48,7 +101,7 @@ def OrderDrink(answers):
 def MixDrink(answers):
     drink = []
     
-    for key, answer in answers.items():## Question items() is a function of some kind? Unsure what it creates.
+    for key, answer in answers.items():## items returns a list of the dicts (key, value) tuple pairs
         
         if (answer == True):
             drink.append(random.choice(ingredients[key]))
@@ -60,12 +113,13 @@ def MixDrink(answers):
             
 if __name__ == '__main__':
     
-    OrderDrink(answers)
-
-    #print(answers)
+    answers=OrderDrink()
 
     drink = MixDrink(answers) 
+    
+    drink_name = RandomDrinkName()
+    
     #Variables cannot be accessed outside the scope of a function they were defined in.
     #so we create a variable with the same name outside of the MixDrink() and print that
+    print("Arrr. One "+drink_name+" coming right up... Her ye arrrr... Drink up ya scurvy bum!")
     print("Drink will contain: "+str(drink))
-    #print(drink)
